0ddd739efdc3411cf504d3991e9a29475aacdf0f
phillipdavidstearns <phil@phillipstearns.com>
2020-03-21 22:05:50 -0400

    handling received data as strings and chars

--- the_forest.py
+++ the_forest.py
@@ -102,25 +102,20 @@ def main():
 	#-------------------------------------------------------------------
 	# argument stuff
 	ap = argparse.ArgumentParser()
-	# ap.add_argument("-s", "--socket-blocking", action='store_true', default=False, required=False, help="non-blocking by default")
 	ap.add_argument("-i", "--interface", default="wlan0", required=False, help="[if]")
 	ap.add_argument("-l","--lhost-ip", default='', required=False, help="LHOST IP")
 	ap.add_argument("-p","--lhost-port", type=int, default=31337, required=False, help="LHOST PORT")
 	ap.add_argument("-c", "--chunk-size", type=float, default=2048, required=False, help="chunk size in frames") # not sure if I need this
 	ap.add_argument("-r", "--frame-rate", type=float, default=30, required=False, help="frames per second")
-	# ap.add_argument("-t", "--timeout", type=float, default=0.0, required=False, help="socket timeout in seconds")
 	ap.add_argument("-b", "--frame-size", type=int, default=4, required=False, help="number of bytes to display per frame")
 	ap.add_argument('-v', "--verbose", action='store_true', default=False, help='Verbose mode. Display debug messages')
 	args = ap.parse_args()
 
-	# SOCKET_BLOCKING = args.socket_blocking
 	IFACE = args.interface
 	CHUNK = args.chunk_size
 	RATE = args.frame_rate
 	BYTES = args.frame_size
-	verbose=args.verbose
-	# future: make arguments for these
-	# TIMEOUT=10 
+	VERBOSE=args.verbose
 	HOST = args.lhost_ip
 	PORT = args.lhost_port
 
@@ -128,10 +123,10 @@ def main():
 	#	verbose or debug mode
 
 	def debug(message):
-		if verbose:
+		if VERBOSE:
 			print(message)
 
-	if verbose:
+	if VERBOSE:
 		debug("Verbose mode. Displaying debug messeges")
 
 	if args.timeout > 0.0:
