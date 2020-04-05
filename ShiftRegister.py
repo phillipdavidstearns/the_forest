dcab1bf6eb8dfadc26c3f9fbf69f2d41202b7549
phillipdavidstearns <phil@phillipstearns.com>
2020-04-04 21:33:58 -0400

    added code for imminent_shift will have to break out later...

---
+++ ShiftRegister.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+
+class ShiftRegister:
+
+	def __init__(self, channels=32, tap1=16, tap1_en=True, tap2=15, tap2_en=True):
+		self.register = [0]*channels
+		self.tap1 = tap1
+		self.tap1_en = tap1_en
+		self.tap2 = tap2
+		self.tap2_en = tap2_en
+
+	def set(self, Q):
+		if len(Q) == len(self.register):
+			self.register = Q
+
+	def setBit(self, Q, val):
+		if Q < len(self.register):
+			if val == 0:
+				self.register[Q] = 0
+			else:
+				self.register[Q] = 1
+
+	def getBit(self, Q):
+		if Q < len(self.register):
+			return self.register[Q]
+		else:
+			return 0
+
+	def shiftLeft(self, data=0):
+		temp = [0] * len(self.register)
+		for i in range(len(self.register)):
+			if i > 0:
+				temp[i] = self.register[i-1]
+			elif i <= 0:
+				temp[i] = data
+		self.register = temp
+		return self.register
+
+	def shiftRight(self, data=0):
+		temp = [0] * len(self.register)
+		for i in range(len(self.register)):
+			if i < len(self.register)-1:
+				temp[i]=self.register[i+1]
+			elif i == len(self.register)-1:
+				temp[i] = data
+		self.register = temp
+		return self.register
+
+	def lfsrLeft(self):
+		t1 = self.register[self.tap1] & self.tap1_en
+		t2 = self.register[self.tap2] & self.tap2_en
+		data = t1 ^ t2
+		self.shiftLeft(data)
+		return self.register
+
+	def lfsrRight(self):
+		t1 = self.register[self.tap1] & self.tap1_en
+		t2 = self.register[self.tap2] & self.tap2_en
+		data = t1 ^ t2
+		self.shiftRight(data)
+		return self.register
---
+++ imminent_shift_4.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python3
+
+#------------------------------------------------------
+#
+# the_forest.py
+#
+# A script to translate network traffic into lighting control
+# should be run on a raspberry pi.
+# Raspberry Pi needs to be connected to a network
+# Raspberry Pi GPIO is used to output controlsignals
+# via a 4094 shift register
+# The output of the shift register can be used to trigger
+# devices connected to Solid State Relays.
+
+import os
+import sys
+import subprocess
+import argparse
+from signal import *
+import time
+import select
+import CD4094 as IO
+from ShiftRegister import ShiftRegister
+
+#------------------------------------------------------------------------
+#
+
+def bytes_to_bits(data, channels):
+	channelStates=[]
+	for i in range(len(data)):
+		b = data[i]
+		for j in range(8):
+			channelStates.append(b >> j & 1)
+	return channelStates
+
+#------------------------------------------------------------------------
+#
+
+def shutdown():
+	IO.stop()
+	sys.exit(0)
+
+#------------------------------------------------------------------------
+#	Signal Interrupt/Terminate Handlers
+
+# catch control+c
+def SIGINT_handler(sig, frame):
+	shutdown()
+
+# catch termination signals from the system
+def SIGTERM_handler(sig, frame):
+	shutdown()
+
+#------------------------------------------------------------------------
+# main
+
+def main():
+
+	lfsr = ShiftRegister()
+
+	if os.getuid() != 0:
+		print("Must be run as root.")
+		sys.exit(1)
+
+	# interrupt and terminate signal handling
+	signal(SIGINT, SIGINT_handler)
+	signal(SIGTERM, SIGTERM_handler)
+
+	#-------------------------------------------------------------------
+	# argument stuff
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-r", "--frame-rate", type=float, default=30, required=False, help="frames per second")
+	ap.add_argument('-v', "--verbose", action='store_true', default=False, help='Verbose mode. Display debug messages')
+	args = ap.parse_args()
+
+	RATE = args.frame_rate
+	VERBOSE = args.verbose
+
+	#------------------------------------------------------------------------
+	#	verbose or debug mode
+
+	def debug(message):
+		if VERBOSE:
+			print(message)
+
+	if VERBOSE:
+		debug("Verbose mode. Displaying debug messeges")
+
+	# initalize TCP socket
+	# from example at https://docs.python.org/3.7/library/socket.html#example
+	
+	channels = 32 # number of output channels
+
+	# Pin assignments
+	# outputs
+	strobe = 17 # latch strobe GPIO pin
+	data = 27 # data GPIO pin
+	clock = 22 # clock GPIO pin
+	enable = 23 # IOister enable GPIO pin
+	# make composite lists to pass along to IO
+	pins = [ strobe, data, clock, enable ]
+	IO.init(pins, channels)
+
+	while True:
+		start_time = time.time()
+		output=lfsr.leftShift()
+		print(output)
+		IO.update(output)
+		stop_time = time.time()
+		time.sleep((1/RATE) - ( stop_time - start_time ))
+
+if __name__ == '__main__':
+	main()
